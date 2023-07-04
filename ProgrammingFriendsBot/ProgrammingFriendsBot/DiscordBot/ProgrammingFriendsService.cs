36ebca0d27b2caf7dab6177d1d7055fe5b01e167
Kyle Givler <xvegan88x@gmail.com>
2023-07-04 18:49:57 -0400

    Add new users to the programmers role

--- ProgrammingFriendsBot/ProgrammingFriendsBot/DiscordBot/ProgrammingFriendsService.cs
+++ ProgrammingFriendsBot/ProgrammingFriendsBot/DiscordBot/ProgrammingFriendsService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using ProgrammingFriendsBot.Common.Options;
+using System.Text;
 
 namespace ProgrammingFriendsBot.DiscordBot;
 
@@ -22,8 +23,31 @@ internal class ProgrammingFriendsService : IHostedService
             Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
         });
 
+        discord.GuildMemberAdded += async (s, e) => {
+            var programmersRole = e.Guild.Roles
+                .Where(r => r.Value.Name == "Programmers")
+                .Select(r => r.Value)
+                .FirstOrDefault();
+
+            if (programmersRole is not null)
+            {
+                await e.Member.GrantRoleAsync(programmersRole, "User Joined The Server");
+            }
+
+            var generalChannel = e.Guild.Channels
+                .Where(c => c.Value.Name
+                .Equals("general", StringComparison.InvariantCultureIgnoreCase))
+                .Select(c => c.Value)
+                .FirstOrDefault();
+
+            if (generalChannel is not null)
+            {
+                await generalChannel.SendMessageAsync($"Everyone please welcome {e.Member.Nickname}!");
+            }
+        };
+
         discord.MessageCreated += async (s, e) => {
-            if (e.Message.Content.ToLower().StartsWith("ping"))
+            if (e.Message.Content.ToLower().StartsWith("$ping"))
                 await e.Message.RespondAsync("pong!");
         };
 
