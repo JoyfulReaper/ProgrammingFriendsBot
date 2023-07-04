046402f92fb4ed8141b85b267e08ea655eecde74
Kyle Givler <xvegan88x@gmail.com>
2023-07-04 17:16:13 -0400

    Initial Project Setup

---
+++ ProgrammingFriendsBot/ProgrammingFriendsBot/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProgrammingFriendsBot.DiscordBot;
+
+namespace ProgrammingFriendsBot;
+
+internal static class DependencyInjection
+{
+    internal static IHost SetupDependencyInjection(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) => {
+                services.AddHostedService<ProgrammingFriendsService>();
+            }).ConfigureAppConfiguration((context, config) => {
+                config.AddUserSecrets<Program>();
+            }).Build();
+
+        return host;
+    }
+}
---
+++ ProgrammingFriendsBot/ProgrammingFriendsBot/DiscordBot/ProgrammingFriendsService.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+
+namespace ProgrammingFriendsBot.DiscordBot;
+
+internal class ProgrammingFriendsService : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var discord = new DiscordClient(new DiscordConfiguration() {
+            Token = "TOKENHEREFORNOW",
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+        });
+
+        discord.MessageCreated += async (s, e) => {
+            if (e.Message.Content.ToLower().StartsWith("ping"))
+                await e.Message.RespondAsync("pong!");
+        };
+
+        await discord.ConnectAsync();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
